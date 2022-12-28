367e4b350049d01913234bb0b1a74f173539bd19
Jagrath <jagrathshettylxix@gmail.com>
2022-12-28 19:24:21 +0530

    added hard coded prop colors

--- Components/ProductCard.tsx
+++ Components/ProductCard.tsx
@@ -5,18 +5,58 @@ import { OrbitControls, useGLTF } from "@react-three/drei";
 
 type Props = {};
 
-export function Model(props: Props) {
+type Props1 = {
+  customColors: {
+    mesh: string;
+    stripes: string;
+    sole: string;
+  };
+};
+
+function Model(props: Props1) {
   const { nodes, materials } = useGLTF("/gltf/shoe.gltf");
   return (
     <group {...props} dispose={null} scale={3}>
-      <mesh geometry={nodes.shoe.geometry} material={materials.laces} />
-      <mesh geometry={nodes.shoe_1.geometry} material={materials.mesh} />
-      <mesh geometry={nodes.shoe_2.geometry} material={materials.caps} />
-      <mesh geometry={nodes.shoe_3.geometry} material={materials.inner} />
-      <mesh geometry={nodes.shoe_4.geometry} material={materials.sole} />
-      <mesh geometry={nodes.shoe_5.geometry} material={materials.stripes} />
-      <mesh geometry={nodes.shoe_6.geometry} material={materials.band} />
-      <mesh geometry={nodes.shoe_7.geometry} material={materials.patch} />
+      <mesh
+        geometry={nodes.shoe.geometry}
+        material={materials.laces}
+        material-color={"blue"}
+      />
+      <mesh
+        geometry={nodes.shoe_1.geometry}
+        material={materials.mesh}
+        material-color={props.customColors.mesh}
+      />
+      <mesh
+        geometry={nodes.shoe_2.geometry}
+        material={materials.caps}
+        material-color={"blue"}
+      />
+      <mesh
+        geometry={nodes.shoe_3.geometry}
+        material={materials.inner}
+        material-color={"blue"}
+      />
+      <mesh
+        geometry={nodes.shoe_4.geometry}
+        material={materials.sole}
+        material-color={props.customColors.sole}
+      />
+      <mesh
+        geometry={nodes.shoe_5.geometry}
+        material={materials.stripes}
+        material-color={props.customColors.stripes}
+      />
+      <mesh
+        geometry={nodes.shoe_6.geometry}
+        material={materials.band}
+        material-color={"blue"}
+      />
+      <mesh
+        geometry={nodes.shoe_7.geometry}
+        material={materials.patch}
+        material-color={"blue"}
+      />
     </group>
   );
 }
@@ -25,26 +65,40 @@ const ProductCard = (props: Props) => {
   return (
     <div className="min-h-screen  flex items-center justify-around bg-neutral-900 text-white">
       <div className="min-h-[50vh] rounded-xl p-8 min-w-[35vw] bg-zinc-800">
-        <div className="product-canvas min-w-[35vw] h-[38vh] bg-white shadow-xl rounded-none mb-5">
+        <div className="product-canvas min-w-[35vw] h-[38vh] bg-zinc-600 shadow-xl rounded-none mb-5">
           <Canvas>
             <ambientLight />
-            <Model />
+            <spotLight
+              intensity={0.9}
+              angle={0.1}
+              penumbra={1}
+              position={[10, 15, 10]}
+              castShadow
+            />
+            <Model
+              customColors={{ mesh: "pink", stripes: "blue", sole: "white" }}
+            />
+            <OrbitControls
+              enablePan={true}
+              enableZoom={true}
+              enableRotate={true}
+            />
           </Canvas>
         </div>
         <h2>Color chooser</h2>
         <div className="flex flex-row">
           <div className="p-2">
-            <input type="color" id="head" name="head" value="#e66465" />
-            <label htmlFor="head">Main</label>
+            <input type="color" id="mesh" name="mesh" value="#e66465" />
+            <label htmlFor="mesh">Main</label>
           </div>
 
           <div className="p-2">
-            <input type="color" id="body" name="body" value="#f6b73c" />
-            <label htmlFor="body">Stripes</label>
+            <input type="color" id="stripes" name="stripes" value="#f6b73c" />
+            <label htmlFor="stripes">Stripes</label>
           </div>
           <div className="p-2">
-            <input type="color" id="body" name="body" value="#f6b73c" />
-            <label htmlFor="body">Soul</label>
+            <input type="color" id="sole" name="sole" value="#f6b73c" />
+            <label htmlFor="sole">Soul</label>
           </div>
         </div>
       </div>
