af30f8b755a6799b22788711fdc797fe4511f0c7
Jagrath <jagrathshettylxix@gmail.com>
2022-12-28 19:14:16 +0530

    added shoe model

--- Components/ProductCard.tsx
+++ Components/ProductCard.tsx
@@ -5,12 +5,31 @@ import { OrbitControls, useGLTF } from "@react-three/drei";
 
 type Props = {};
 
+export function Model(props: Props) {
+  const { nodes, materials } = useGLTF("/gltf/shoe.gltf");
+  return (
+    <group {...props} dispose={null} scale={3}>
+      <mesh geometry={nodes.shoe.geometry} material={materials.laces} />
+      <mesh geometry={nodes.shoe_1.geometry} material={materials.mesh} />
+      <mesh geometry={nodes.shoe_2.geometry} material={materials.caps} />
+      <mesh geometry={nodes.shoe_3.geometry} material={materials.inner} />
+      <mesh geometry={nodes.shoe_4.geometry} material={materials.sole} />
+      <mesh geometry={nodes.shoe_5.geometry} material={materials.stripes} />
+      <mesh geometry={nodes.shoe_6.geometry} material={materials.band} />
+      <mesh geometry={nodes.shoe_7.geometry} material={materials.patch} />
+    </group>
+  );
+}
+
 const ProductCard = (props: Props) => {
   return (
     <div className="min-h-screen  flex items-center justify-around bg-neutral-900 text-white">
       <div className="min-h-[50vh] rounded-xl p-8 min-w-[35vw] bg-zinc-800">
         <div className="product-canvas min-w-[35vw] h-[38vh] bg-white shadow-xl rounded-none mb-5">
-          <Canvas></Canvas>
+          <Canvas>
+            <ambientLight />
+            <Model />
+          </Canvas>
         </div>
         <h2>Color chooser</h2>
         <div className="flex flex-row">
